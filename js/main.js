abf13cf27f5b5e1c1662952fe890ec87b4c582f0
Santosh Marigowda <santosharakere@gmail.com>
2018-11-18 16:45:20 +0000

    a simple model

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@
 // In the first few sections, we do all the coding here.
 // Later, you'll see how to organize your code into separate
 // files and modules.
+
+const Song = Backbone.Model.extend({
+  initialize: function() {
+    console.log('A new song has been created.');
+  }
+});
+
+const song = new Song();
