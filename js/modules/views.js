c1cb2064b6728b9c020c2a1d41113f5471aec75c
Santosh Marigowda <santosharakere@gmail.com>
2018-11-20 05:08:02 +0000

    passing model to view

--- js/modules/views.js
+++ js/modules/views.js
@@ -1,17 +1,18 @@
+const Song = Backbone.Model.extend();
+
 const SongView = Backbone.View.extend({
   tagName: 'span',
-  className: 'song',
-  id: '1234',
-  attributes: {
-    'data-test-id': 'test-id-song'
-  },
   render: function() {
-    this.$el.html("Hello World!");
+    this.$el.html(this.model.get('title'));
     return this;
   }
 });
 
+const song = new Song({ title: 'Blue in Green' });
+
 // const songView = new SongView({ el: '#container' });
-const songView = new SongView();
+const songView = new SongView({
+  model: song
+});
 
 $('#container').html(songView.render().$el);
