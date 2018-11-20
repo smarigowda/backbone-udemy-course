d3fbbd2ab21545dd9fe0633a34e2b98bbeb91f9f
Santosh Marigowda <santosharakere@gmail.com>
2018-11-20 05:26:57 +0000

    rendering views using collection

--- js/modules/views.js
+++ js/modules/views.js
@@ -1,18 +1,42 @@
 const Song = Backbone.Model.extend();
+const Songs = Backbone.Collection.extend({
+  model: Song
+});
 
 const SongView = Backbone.View.extend({
-  tagName: 'span',
+  tagName: 'li',
   render: function() {
     this.$el.html(this.model.get('title'));
     return this;
   }
 });
 
-const song = new Song({ title: 'Blue in Green' });
-
-// const songView = new SongView({ el: '#container' });
-const songView = new SongView({
-  model: song
+const SongsView = Backbone.View.extend({
+  render: function() {
+    this.model.each(song => {
+      const songView = new SongView({ model: song });
+      this.$el.append(songView.render().$el);
+    });
+  }
 });
 
-$('#container').html(songView.render().$el);
+
+const songs = new Songs([
+  new Song({ title: 'Blue is Green' }),
+  new Song({ title: 'Green is Yellow' }),
+  new Song({ title: 'All Yellow' }),
+])
+
+const songsView = new SongsView({ el: '#container', model: songs });
+songsView.render();
+
+
+
+// const song = new Song({ title: 'Blue in Green' });
+
+// const songView = new SongView({ el: '#container' });
+// const songView = new SongView({
+//   model: song
+// });
+
+// $('#container').html(songView.render().$el);
