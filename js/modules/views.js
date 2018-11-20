4bf81d239ec0570c3db10bc67abfd1a4799d5a09
Santosh Marigowda <santosharakere@gmail.com>
2018-11-20 04:59:02 +0000

    enhancing view

--- js/modules/views.js
+++ js/modules/views.js
@@ -1,9 +1,18 @@
 const SongView = Backbone.View.extend({
+  tagName: 'span',
+  className: 'song',
+  id: '1234',
+  attributes: {
+    'data-test-id': 'test-id-song'
+  },
   render: function() {
     this.$el.html("Hello World!");
     return this;
   }
 });
 
-const songView = new SongView({ el: '#container' });
+// const songView = new SongView({ el: '#container' });
+const songView = new SongView();
 songView.render();
+
+$('#container').html(songView.$el);
