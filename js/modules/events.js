4e4ceb783751db30105c62fdd5b6ae3cc601b6a0
Santosh Marigowda <santosharakere@gmail.com>
2018-11-26 05:41:05 +0000

    passing data via event

--- js/modules/events.js
+++ js/modules/events.js
@@ -1,14 +1,17 @@
 const person = {
   name: 'Santosh Marigowda',
   walk: function() {
-    this.trigger('walking');
+    this.trigger('walking', {
+      speed: 1,
+      startTime: '08:00'
+    });
   }
 }
 
 _.extend(person, Backbone.Events);
 
-person.on('walking', function() {
-  console.log('walking event triggered...');
+person.on('walking', function(e) {
+  console.log('walking event triggered...', e);
 })
 
 person.walk();
